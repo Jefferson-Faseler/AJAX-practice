dca3c1b07a7ac61b259c9dd5dc95fb4d089d12bc
Stephen Grayson <fingrayson@gmail.com>
2017-01-14 21:25:25 -0500

    Added more logic.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,7 +3,25 @@ get '/' do
 end
 
 post '/roll' do
-  dice = Dice.new(params[:dice][:size].to_i)
-  @dice = dice.roll
+
+@total1 = 0
+@total2 = 0
+@total3 = 0
+
+params[:dice][:number][:first].to_i.times do
+  dice = Dice.new(params[:dice][:size][:first].to_i)
+  @total1 = dice.roll + @total1
+end
+
+params[:dice][:number][:second].to_i.times do
+  dice = Dice.new(params[:dice][:size][:second].to_i)
+  @total2 = dice.roll + @total2
+end
+
+params[:dice][:number][:third].to_i.times do
+  dice = Dice.new(params[:dice][:size][:third].to_i)
+  @total3 = dice.roll + @total3
+end
+
   erb :index
 end
